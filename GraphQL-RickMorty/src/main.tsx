fc67504f96dfb1f35d4ba903bda0a262a5926793
Jordy <jper220803@gmail.com>
2025-02-24 12:10:28 +0100

    init apollo

--- GraphQL-RickMorty/src/main.tsx
+++ GraphQL-RickMorty/src/main.tsx
@@ -2,9 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql',
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query {
+        characters {
+          results {
+            name
+            image
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ApolloProvider client={client}>
+      <StrictMode>
+        <App />
+      </StrictMode>,
+  </ApolloProvider>
+
 )
